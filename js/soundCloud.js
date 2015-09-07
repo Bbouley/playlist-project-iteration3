ef7b8c1af03a4faf873167bee93cf04df2b78467
bbouley <b.bouley@hotmail.co.uk>
2015-09-06 21:20:05 -0600

    added some changes to soundcloud function

--- js/soundCloud.js
+++ js/soundCloud.js
@@ -1,11 +1,11 @@
-window.onload = function(){
+// window.onload = function(){
 
-  SC.initialize({
-    client_id: '3a76cf73e430887c16e9897fcf630bcc'
-  });
+//   SC.initialize({
+//     client_id: '3a76cf73e430887c16e9897fcf630bcc'
+//   });
 
-  var track_url = 'https://soundcloud.com/calyxteebee/sawn-off';
-    SC.oEmbed(track_url, { auto_play: false, maxheight: 100 }, document.getElementById('player'));
+//   var track_url = 'https://soundcloud.com/calyxteebee/sawn-off';
+//     SC.oEmbed(track_url, { auto_play: false, maxheight: 100 }, document.getElementById('player'));
 
 
-};
+// };
